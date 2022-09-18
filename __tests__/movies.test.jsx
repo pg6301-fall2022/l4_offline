70812f374a01627f5939a8e3632ab6ba49a1e86e
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-09-18 14:24:46 +0200

    Step 2 - starter app and basic test

---
+++ __tests__/movies.test.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import {ListMovies} from "../application";
+
+describe ("movies application", () => {
+    it("shows movie list", () => {
+        const element = document.createElement("div");
+        ReactDOM.render(<ListMovies/>, element);
+        expect(element.querySelector("h1").innerHTML)
+            .toEqual("List movies");
+        expect(element.innerHTML).toMatchSnapshot();
+    })
+    })
---
+++ application.jsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export function ListMovies(){
+    return <h1>List movies</h1>;
+}
